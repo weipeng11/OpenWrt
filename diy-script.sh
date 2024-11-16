8818fbeb8eb0d5a462e4ab53db3b1ec636679bd1
weipeng11 <95176557+weipeng11@users.noreply.github.com>
2024-11-16 13:23:55 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -39,7 +39,7 @@ git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-a
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
 # git_sparse_clone master https://github.com/syb999/openwrt-19.07.1 package/network/services/msd_lite
-
+git clone --depth=1 https://github.com/jeessy2/ddns-go package/luci-app-ddnsgo
 # 科学上网插件
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
